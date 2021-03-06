98f8eaa4c50490845112cd507bcb044e51458d74
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-03-06 11:03:23 -0500

    comment out warnings to fix deployment issues

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+// import axios from "axios";
 import { Container, Row, Col } from "react-bootstrap";
 
 export default function Forecast() {
-  const [temperature, setTemperature] = useState(null)
-  function displayForecast(response) {
+  // const [temperature, setTemperature] = useState(null)
+  // function displayForecast(response) {
 
-  }
+  // }
 
-  const forecastKey = "13ce2da2cd2d4da6b6a30677bd0ecc0d";
-  let city = `Atlanta`
-  const forecastUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${forecastKey}&units=I`;
+  // const forecastKey = "13ce2da2cd2d4da6b6a30677bd0ecc0d";
+  // let city = `Atlanta`
+  // const forecastUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${forecastKey}&units=I`;
 
-  axios.get(forecastUrl).then(displayForecast);
+  // axios.get(forecastUrl).then(displayForecast);
 
   return (
     <Container className="forecast">
