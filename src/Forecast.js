3c5c5b19c023ff41f34b5c365d2f483e229236ae
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2021-03-11 22:31:49 -0500

    added use state, pulled live data from API

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,78 +1,143 @@
-import React from "react";
-// import axios from "axios";
+import React, { useState } from "react";
+import axios from "axios";
 import { Container, Row, Col } from "react-bootstrap";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 export default function Forecast() {
-  // const [temperature, setTemperature] = useState(null)
-  // function displayForecast(response) {
+  const [ready, setReady] = useState(false);
 
-  // }
+  // sunday
+  const [sunHiTemp, setSunHiTemp] = useState(null);
+  const [sunLoTemp, setSunLoTemp] = useState(null);
+  // monday
+  const [monHiTemp, setMonHiTemp] = useState(null);
+  const [monLoTemp, setMonLoTemp] = useState(null);
+  //tuesday
+  const [tueHiTemp, setTueHiTemp] = useState(null);
+  const [tuesLoTemp, setTuesLoTemp] = useState(null);
+  //wednesday
+  const [wedHiTemp, setWedHiTemp] = useState(null);
+  const [wedLoTemp, setwedLoTemp] = useState(null);
+  //thursday
+  const [thurHiTemp, setThurHiTemp] = useState(null);
+  const [thurLoTemp, setThurLoTemp] = useState(null);
+  //friday
+  const [friHiTemp, setFriHiTemp] = useState(null);
+  const [friLoTemp, setFriLoTemp] = useState(null);
+  //saturday
+  const [satHiTemp, setSatHiTemp] = useState(null);
+  const [satLoTemp, setSatLoTemp] = useState(null);
 
-  // const forecastKey = "13ce2da2cd2d4da6b6a30677bd0ecc0d";
-  // let city = `Atlanta`
-  // const forecastUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${forecastKey}&units=I`;
+  function displayForecast(response) {
+    //sunday
+    setSunHiTemp(Math.round(response.data.data[0].app_max_temp));
+    setSunLoTemp(Math.round(response.data.data[0].app_min_temp));
 
-  // axios.get(forecastUrl).then(displayForecast);
+    //monday
+    setMonHiTemp(Math.round(response.data.data[1].app_max_temp));
+    setMonLoTemp(Math.round(response.data.data[1].app_min_temp));
 
-  return (
-    <Container className="forecast">
-      <Row>
-        <Col xs={4} className="day-box">
-          <strong className="day">Sun</strong>
-          <br />
-          55° / 55°
-          <br />
-          icon
-        </Col>
-        <Col xs={4} className="day-box">
-          <strong className="day">Mon</strong>
-          <br />
-          55° / 55°
-          <br />
-          icon
-        </Col>
-        <Col xs={4} className="day-box">
-          <strong className="day">Tues</strong>
-          <br />
-          55° / 55°
-          <br />
-          icon
-        </Col>
-      </Row>
-      <br />
-      <Row>
-        <Col xs={4} className="day-box">
-          <strong className="day">Wed</strong>
-          <br />
-          55° / 55°
-          <br />
-          icon
-        </Col>
-        <Col xs={4} className="day-box">
-          <strong className="day">Thurs</strong>
-          <br />
-          55° / 55°
-          <br />
-          icon
-        </Col>
-        <Col xs={4} className="day-box">
-          <strong className="day">Fri</strong>
-          <br />
-          55° / 55°
-          <br />
-          icon
-        </Col>
-      </Row>
-      <br />
-      <Row>
-        <Col xs={4} className="day-box">
-          <strong className="day">Sat</strong>
-          <br />
-          55° / 55°
-          <br />
-          icon
-        </Col>
-      </Row>
-    </Container>
-  );
+    //tuesday
+    setTueHiTemp(Math.round(response.data.data[2].app_max_temp));
+    setTuesLoTemp(Math.round(response.data.data[2].app_min_temp));
+
+    //wednesday
+    setWedHiTemp(Math.round(response.data.data[3].app_max_temp));
+    setwedLoTemp(Math.round(response.data.data[3].app_min_temp));
+
+    //thursday
+    setThurHiTemp(Math.round(response.data.data[4].app_max_temp));
+    setThurLoTemp(Math.round(response.data.data[4].app_min_temp));
+
+    //friday
+    setFriHiTemp(Math.round(response.data.data[5].app_max_temp));
+    setFriLoTemp(Math.round(response.data.data[5].app_min_temp));
+
+    //saturday
+    setSatHiTemp(Math.round(response.data.data[6].app_max_temp));
+    setSatLoTemp(Math.round(response.data.data[6].app_min_temp));
+
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <Container className="forecast">
+        <Row>
+          <Col xs={4} className="day-box">
+            <strong className="day">Sun</strong>
+            <br />
+            {sunHiTemp}° / {sunLoTemp}°
+            <br />
+            icon
+          </Col>
+          <Col xs={4} className="day-box">
+            <strong className="day">Mon</strong>
+            <br />
+            {monHiTemp}° / {monLoTemp}°
+            <br />
+            icon
+          </Col>
+          <Col xs={4} className="day-box">
+            <strong className="day">Tues</strong>
+            <br />
+            {tueHiTemp}° / {tuesLoTemp}°
+            <br />
+            icon
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col xs={4} className="day-box">
+            <strong className="day">Wed</strong>
+            <br />
+            {wedHiTemp}° / {wedLoTemp}°
+            <br />
+            icon
+          </Col>
+          <Col xs={4} className="day-box">
+            <strong className="day">Thurs</strong>
+            <br />
+            {thurHiTemp}° / {thurLoTemp}°
+            <br />
+            icon
+          </Col>
+          <Col xs={4} className="day-box">
+            <strong className="day">Fri</strong>
+            <br />
+            {friHiTemp}° / {friLoTemp}°
+            <br />
+            icon
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col xs={4} className="day-box">
+            <strong className="day">Sat</strong>
+            <br />
+            {satHiTemp}° / {satLoTemp}°
+            <br />
+            icon
+          </Col>
+        </Row>
+      </Container>
+    );
+  } else {
+    const forecastKey = "13ce2da2cd2d4da6b6a30677bd0ecc0d";
+    let city = `Atlanta`;
+    const forecastUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${forecastKey}&units=I`;
+
+    axios.get(forecastUrl).then(displayForecast);
+
+    return (
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={1000} //1 sec
+      />
+    );
+  }
 }
--- src/WeatherData.js
+++ src/WeatherData.js
@@ -1,30 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col } from "react-bootstrap";
+import axios from "axios";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 export default function WeatherData() {
-  return (
-    <Col className="strong-side">
-      <h6 className="dateTime">
-        <span className="fullDate">Dec 26, 2020</span>
-        <br />
-        <span className="time">13:00</span> <br />
-        <h1 className="city">Atlanta</h1>
-        <h6 id="weather">Sunny</h6>
-        <h2 className="currentTemp">
-          <span id="current-temp">55</span>
-          <span href="#" id="f-link" className="active">
-            °F
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+
+  // error function
+
+  function errorFunction() {
+    alert(
+      "😬 I love Narnia, but that's not a real place either. Please try again!"
+    );
+  }
+
+  function displayTemp(response) {
+    setTemperature(Math.round(response.data.main.temp));
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <Col className="strong-side">
+        <h6 className="dateTime">
+          <span className="fullDate">Dec 26, 2020</span>
+          <br />
+          <span className="time">13:00</span> <br />
+          <h1 className="city">Atlanta</h1>
+          <h6 id="weather">Sunny</h6>
+          <h2 className="currentTemp">
+            <span id="current-temp">{temperature}</span>
+            <span href="#" id="f-link" className="active">
+              °F
+            </span>
+            <span className="line">|</span>
+            <span href="#" id="c-link">
+              °C
+            </span>
+          </h2>
+          <span className="wind-humidity">
+            Wind: 5 mph <br />
+            Humidity: 5%
           </span>
-          <span className="line">|</span>
-          <span href="#" id="c-link">
-            °C
-          </span>
-        </h2>
-        <span className="wind-humidity">
-          Wind: 5 mph <br />
-          Humidity: 5%
-        </span>
-      </h6>
-    </Col>
-  );
+        </h6>
+      </Col>
+    );
+  } else {
+    const apiKey = "7cae0a9d7005e1c52f2d634f98d69293";
+    let city = `Atlanta`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(displayTemp).catch(errorFunction);
+
+    return (
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={1000} //1 sec
+      />
+    );
+  }
 }
